fd58f5fe668760e7246b47ee9ad8fb5942c5238e
Tucker <tudge669@gmail.com>
2023-02-01 14:38:44 -0500

    commit tree 9ee9f35a5da4b4eae93b63b40fbf7d51b26da4f6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+    user = finduser
+    item = user.items
+  else
+    item = Item.all
+  end
+  render json: item, include: :user
   end
 
+  def show 
+    item = finditem
+    render json: item, include: :user
+  end
+
+
+  def create
+    user = finduser
+    item = user.items.create(permit_params)
+    render json: item, status: :created
+  end
+
+
+  private
+  def finduser
+    User.find(params[:user_id])
+  end
+  def finditem
+    Item.find(params[:id])
+  end
+  def permit_params
+    params.permit(:name, :description, :price)
+  end
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
 end
